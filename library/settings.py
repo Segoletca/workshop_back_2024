a620c8307d4d073c452347025abbffd74847c1f8
Segoletca <sergey.noskov.2001@mail.ru>
2024-11-17 20:17:49 +0300

    Connect sqlite

--- library/settings.py
+++ library/settings.py
@@ -77,23 +77,23 @@ WSGI_APPLICATION = 'library.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "magazine_db",
-        "USER": "postgres",
-        "PASSWORD": "mysecretpassword",
-        "HOST": "127.0.0.1",
-        "PORT": "5432",
-    }
-}
-
 # DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "magazine_db",
+#         "USER": "postgres",
+#         "PASSWORD": "mysecretpassword",
+#         "HOST": "127.0.0.1",
+#         "PORT": "5432",
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # Password validation
