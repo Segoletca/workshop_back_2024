33223c76132b1ceace1fecc7c84c365f0a04dc48
Segoletca <sergey.noskov.2001@mail.ru>
2024-11-17 23:02:36 +0300

    Create models for papers app

--- papers/models.py
+++ papers/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+from django.contrib.auth.models import User
+
+
 # Create your models here.
+
+class Papers(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.ForeignKey(User, verbose_name='User', on_delete=models.CASCADE,
+                               null=True)
+    content = models.TextField(max_length=2500)
+    time_create = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    class Meta:
+        indexes = [
+            models.Index(fields=["name"]),
+        ]
