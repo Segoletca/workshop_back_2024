0db36b29c181480f1e988bd2df9b08160cfbd0ff
Segoletca <sergey.noskov.2001@mail.ru>
2024-11-18 01:09:52 +0300

    Add a view for ordered list of papers

--- papers/urls.py
+++ papers/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from .views import *
 
 urlpatterns = [
     # path('profile/'),  # Посмотреть список своих статей
-    # path('general/'),  # Посмотреть список всех статей
-    # path('detail/<int:id>'),  # Посмотреть конкретную статью подробно
-    # path('topic/<int:some>')  # Отфильтровать статьи по категориям
+    path('general/', PaperListAPIView.as_view()),  # Посмотреть список всех статей начиная с самой свежей
+    # path('detail/<int:pk>', PaperUpdateAPIView.as_view()),  # Посмотреть/изменить конкретную статью подробно
+    # path('topic/<int:category>', TopicListAPIView.as_view())  # Отфильтровать статьи по категориям
 ]
--- papers/views.py
+++ papers/views.py
@@ -1,3 +1,31 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.shortcuts import render
+from rest_framework import status
+from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveUpdateAPIView
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from .models import Papers, Category
+from .serializers import PapersSerializer
+
 
 # Create your views here.
+class PaperListAPIView(ListAPIView):
+    queryset = Papers.objects.all().order_by('-time_create')
+    serializer_class = PapersSerializer
+
+
+# class PaperUpdateAPIView(RetrieveUpdateAPIView):
+#     queryset = Papers.objects.all().order_by('-time_create')
+#     serializer_class = PapersSerializer
+#
+#
+# class TopicListAPIView(APIView):
+#     def get(self, request, category=None):
+#         try:
+#             category = Category.objects.get(id=category)
+#             papers = Papers.objects.filter(category=category)
+#             serializer = PapersSerializer(papers, many=True)
+#             return Response(serializer.data)
+#         except ObjectDoesNotExist:
+#             return Response({"error": "Category not found"}, status=status.HTTP_404_NOT_FOUND)
